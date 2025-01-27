21311f29860ef80c2e398ea69156e1b4911902e6
Natalia Chiara <natalia@contenidos-digitales.com>
2025-01-27 13:57:01 -0300

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,52 @@
-import FormProduct from "@/components/FormProduct";
+import Image from "next/image";
 import s from "./page.module.css";
 import Link from "next/link";
+import { info } from "@/lib/data";
 
 export default function Home() {
+  const { name, logo, contact, schedule } = info
   return (
     <div className={s.page}>
       <nav className={s.page__nav}>
         <Link href='/' className={s.page__nav__selected}>Homepage</Link>
         <Link href='/productos'>Productos</Link>
-        <Link href='/agregar-productos'>Agregar productos</Link>
       </nav>
-      <main className={s.main}>
-        <FormProduct />
+      <main className={s.page__main}>
+        <div className={s.page__main__info}>
+          <Image src={logo} width={150} height={150} alt="Logo" aria-label="Logo" />
+          <h1>{name}</h1>
+          <div className={s.page__main__info__contact}>
+            {contact.map((item) => {
+              const { label, type, link, icon } = item
+              return (
+                <a target="__blank" key={type} href={link}>
+                  <Image src={icon} width={30} height={30} alt={`${type} icon`} aria-label={`Ir a ${type}`} />
+                  <span>- {label && label}</span>
+                </a>
+              )
+            })}
+          </div>
+          <div className={s.page__main__info__schedule}>
+            <h2>Horarios de Atención:</h2>
+            <ul>
+              {schedule.map((item) => {
+                const { day, hours } = item
+                return (
+                  <li key={day}>
+                    <span>
+                      {day}
+                    </span>
+                    <span>
+                      <Image src='/icons/clock.svg' alt="Clock icon" width={15} height={15} aria-label="Clock icon" />
+                      {hours === '' ? 'Cerrado' : hours}
+                    </span>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+
       </main>
     </div>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -59,3 +59,58 @@ export const sections = [
     slug: 'bebidas'
   }
 ]
+
+export const info = {
+  name: 'Celsius Bar Tigre',
+  logo: '/images/logo.webp',
+  contact: [
+    {
+      label: 'Av. Cazon 641, Tigre, Buenos Aires, Argentina',
+      type: 'location',
+      icon: '/icons/location.svg',
+      link: 'https://maps.app.goo.gl/TiW15wEqftSDZKJ77'
+    },
+    {
+      label: '+54 11 51509394',
+      type: 'Whatsapp',
+      icon: '/icons/whatsapp.svg',
+      link: 'https://api.whatsapp.com/send?phone=541151509394&text=%F0%9F%91%8B%20Hola%2C%20vengo%20de%20https%3A%2F%2Fcelsius-bar.ola.click%20%0ADeseo%20realizar%20el%20siguiente%20pedido%3A%0A'
+    },
+    {
+      label: '@celsius.bar',
+      type: 'Instagram',
+      icon: '/icons/instagram.svg',
+      link: 'https://www.instagram.com/celsius.bar/?hl=es'
+    }
+  ],
+  schedule: [
+    {
+      day: 'Lunes',
+      hours: ''
+    },
+    {
+      day: 'Martes',
+      hours: '18:00 - Medianoche'
+    },
+    {
+      day: 'Miercoles',
+      hours: '18:00 - Medianoche'
+    },
+    {
+      day: 'Jueves',
+      hours: '18:00 - Medianoche'
+    },
+    {
+      day: 'Viernes',
+      hours: '18:00 - Medianoche'
+    },
+    {
+      day: 'Sabado',
+      hours: '18:00 - Medianoche'
+    },
+    {
+      day: 'Domingo',
+      hours: '18:00 - Medianoche'
+    }
+  ]
+}
